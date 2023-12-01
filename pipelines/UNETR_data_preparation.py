8fc108bb6e95d38e17417da8694c12378516ced4
JoaoPeriquito <joao.s.periquito@gmail.com>
2023-12-01 10:43:41 +0000

    fixed bug

--- pipelines/UNETR_data_preparation.py
+++ pipelines/UNETR_data_preparation.py
@@ -19,21 +19,24 @@ def main(username, password, path, df, series_desc):
         folder = db.database(path=pathScan)
         rename.main(folder)
 
+        array_list = []
         for dixon in series_desc:
             try:
                 series_temp = folder.series(SeriesDescription=dixon)
-
-                array_list = []
                 if series_temp:
                     array_temp, header_temp = series_temp[0].array(['SliceLocation', 'AcquisitionTime'], pixels_first=True)
                     array_temp = np.squeeze(array_temp)
                     array_list.append(array_temp)
-                    
-                stacked_array = np.concatenate(array_list, axis=3)
-                nifti_image = nib.Nifti1Image(stacked_array, affine=np.eye(4))
-                nib.save(nifti_image, path + '\\' + 'row'+'.nii.gz')
-            
             except:
-                print(dixon + ' was NOT found!')
+                print(dixon + " was NOT found!")
+
+        try:        
+            stacked_array = np.stack(array_list, axis=3)
+            nifti_image = nib.Nifti1Image(stacked_array, affine=np.eye(4))
+            nib.save(nifti_image, path + '\\' + row +'.nii.gz')
+        except:
+            print('Data was NOT saved!')
+            
+
         
         shutil.rmtree(pathScan)
